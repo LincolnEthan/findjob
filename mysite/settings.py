dd0bd9745f8f594dbd3aa1c1f950c9c0d6f1a8fa
ethan <445366464@qq.com>
2017-02-13 12:12:34 +0800

    downtime server

--- mysite/settings.py
+++ mysite/settings.py
@@ -123,7 +123,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
-LANGUAGE_CODE = 'zh_Hans'
+LANGUAGE_CODE = 'en_us'
 
 TIME_ZONE = 'UTC'
 
